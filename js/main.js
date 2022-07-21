fffc4bfcce572290cbe54421604d1e79a598660a
unknown <jonathanjonyjonathanjoelvos@gmail.com>
2022-07-21 11:33:56 -0300

    localStorage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+let itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+itens.forEach(element => {
+    criarElemento(element);
+});
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log(event.target.elements["nome"].value);
-    console.log(event.target.elements["quantidade"].value);
+    let nome = event.target.elements["nome"].value;
+    let quantidade = event.target.elements["quantidade"].value;
+
+    let itemAtual = {
+        nome: nome,
+        quantidade: quantidade
+    }
+
+    criarElemento(itemAtual);
+
+    itens.push(itemAtual);
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    form.reset();
+})
+
+function criarElemento(item) {
+
+    let li = document.createElement("li");
+    li.classList.add("item");
+
+    let strong = document.createElement("strong");
+    strong.textContent = item.quantidade;
+    li.appendChild(strong);
+    li.innerHTML += item.nome;
+
+    lista.appendChild(li);
+
+    /* console.log(localStorage.key(0)) */ // retorna itens
+
 }
-)
